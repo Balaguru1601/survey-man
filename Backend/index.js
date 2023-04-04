21b82a8161460a50d71dfecf693ab1bed27f5e69
Balaguru <balagurusethuraman@gmail.com>
2023-04-04 16:40:46 +0530

    commit 6.0

--- Backend/index.js
+++ Backend/index.js
@@ -26,11 +26,12 @@ mongoose
 		console.log(err);
 	});
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "https://survey-man.vercel.app/");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept,Authorization-Type"
+		"Origin, X-Requested-With, Content-Type, Accept"
 	);
 	next();
 });
