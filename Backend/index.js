e6a795cecbcf619b697b3e8a57dd5d9de2b72626
Balaguru <balagurusethuraman@gmail.com>
2023-11-19 20:48:37 +0530

    cors fix

--- Backend/index.js
+++ Backend/index.js
@@ -27,8 +27,8 @@ mongoose
 	});
 
 app.use(function (req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "https://survey-man.vercel.app");
-	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+	res.header("Access-Control-Allow-Origin", "https://survey-man.vercel.app");
+	// res.header("Access-Control-Allow-Origin", "http://localhost:5173");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
